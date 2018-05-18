a79a5fe49a0adf6e56e54c21ed0fa396cfda7ebe
AlexKite12 <34075474+AlexKite12@users.noreply.github.com>
2018-05-18 20:13:10 +0300

    DES is ready

--- DES-Algorithm/DES-Algorithm/DES_Algorithm.py
+++ DES-Algorithm/DES-Algorithm/DES_Algorithm.py
@@ -2,21 +2,6 @@ class des():
     def __init__(self):
         self.text = list()
         self.password = list()
-        self.PC_1 = [57, 49, 41, 33, 25, 17, 9,
-                    1, 58, 50, 42, 34, 26, 18,
-                    10, 2, 59, 51, 43, 35, 27,
-                    19, 11, 3, 60, 52, 44, 36,
-                    63, 55, 47, 39, 31, 23, 15,
-                    7, 62, 54, 46, 38, 30, 22,
-                    14, 6, 61, 53, 45, 37, 29,
-                    21, 13, 5, 28, 20, 12, 4]
-
-        self.PC_2 = [14, 17, 11, 24, 1, 5, 3, 28,
-                    15, 6, 21, 10, 23, 19, 12, 4,
-                    26, 8, 16, 7, 27, 20, 13, 2,
-                    41, 52, 31, 37, 47, 55, 30, 40,
-                    51, 45, 33, 48, 44, 49, 39, 56,
-                    34, 53, 46, 42, 50, 36, 29, 32]
 
         self.IP = [58, 50, 42, 34, 26, 18, 10, 2,
                   60, 52, 44, 36, 28, 20, 12, 4,
@@ -108,6 +93,15 @@ class des():
                 51, 45, 33, 48, 44, 49, 39, 56,
                 34, 53, 46, 42, 50, 36, 29, 32]
 
+        self.IP_1 = [40, 8, 48, 16, 56, 24, 64, 32,
+                    39, 7, 47, 15, 55, 23, 63, 31,
+                    38, 6, 46, 14, 54, 22, 62, 30,
+                    37, 5, 45, 13, 53, 21, 61, 29,
+                    36, 4, 44, 12, 52, 20, 60, 28,
+                    35, 3, 43, 11, 51, 19, 59, 27,
+                    34, 2, 42, 10, 50, 18, 58, 26,
+                    33, 1, 41, 9, 49, 17, 57, 25]
+
     #Permut the given block using the given table
     #Перемешивает блок в соотвествии с таблицей
     def permut(self, block, table):
@@ -115,48 +109,74 @@ class des():
 
     """Converting a string into a list of bit"""
     #Преобразует строку в битовую последовательность
-    def string_to_byte(self, text):
-        string = [bin(symb) if isinstance (symb,int) else bin(ord(symb)) for symb in text]
+    def string_to_byte(self, text_original):
+        string = [bin(symb)[2:] if isinstance (symb,int) else bin(ord(symb))[2:] for symb in text_original]
         bitList = list()
         for byte in string:
+            byte = str(byte).zfill(8)
             if len(byte) < 9:
                 for bit in byte:
                     if bit == 'b': 
-                        bit = int('0')
-                    bitList.append(bit)
-            for bit in byte:
-                    if bit == 'b': 
-                        bit = int('0')
-                    bitList.append(bit)
+                        bit = '0'
+                    bitList.append(int(bit))
+            else:
+                for bit in byte:
+                    if bit != 'b': 
+                        bitList.append(int(bit))                    
         return bitList
 
     """Converting a list of bit into a string"""
     #Преобразует битовую последовательность в строку
     def byte_to_string(self,list_of_byte):
-        for strng in list_of_byte:
-            text += chr(int(''.join(strng),2))
-        return text
-
+        text_encrypt = ''
+        for i in range(0, len(list_of_byte), 8):
+            lof = list_of_byte[i:i+8]
+            text_encrypt += chr(int(''.join([str(lof[j]) for j in range(len(lof))]),2))
+        return text_encrypt
 
     def encrypt(self,text,key):
+        text_end = list()
         btlist = self.string_to_byte(text) #преобразует текст в битовую последовательность
-        while len(btlist)//64 != 0: #расширяет битовую последовательность 0, если длина не кратна 64
-            btlist.append(int(0))
+        while len(btlist)%64 != 0: #расширяет битовую последовательность 0, если длина не кратна 64
+            btlist.append(0)
         bit_block = []
         for btBlock in self.seperation_list(btlist, 64):#разбивает битовую последовательность на 64 битовые блоки
-            bit_block.append(self.permut(btBlock, IP))
-        self.password = self.string_to_byte(key)[:56]#преобразует пароль в 56 битовую последовательность
+            bit_block.append(self.permut(btBlock, self.IP))
+        self.password = self.string_to_byte(key[:64])#преобразует пароль в 56 битовую последовательность
+
         for block in bit_block:
-            left,right = seperation_list(block,32)
-            left_list = [left]
-            right_list = [right]
-            for i in range(1,16):
-                left_list.append(right_list[i-1])
-                right_list.append()
+            left_list, right_list = list(), list()
+            left,right = self.seperation_list(block,32)
+            left_list.append(left)
+            right_list.append(right)
+            for i in range(16):
+                left_list.append(right_list[i])            
+                right_list.append(self.xor(left_list[i], self.F_function(right_list[i], self.generate_key(self.password,i))))                
+            text_end.extend(self.permut(self.combine_blocks([left_list[-1],right_list[-1]]), self.IP_1))
+        return self.byte_to_string(text_end)
 
+    def decrypt(self,cipher_text,key):
+        text_end = list()
+        btlist = self.string_to_byte(cipher_text) #преобразует текст в битовую последовательность
+        while len(btlist)%64 != 0: #расширяет битовую последовательность 0, если длина не кратна 64
+            btlist.append(0)
+        bit_block = []
+        for btBlock in self.seperation_list(btlist, 64):#разбивает битовую последовательность на 64 битовые блоки
+            bit_block.append(self.permut(btBlock, self.IP))
+        self.password = self.string_to_byte(key)[:64]#преобразует пароль в 64 битовую последовательность
 
-            #for i in range(1,17):
-                
+        for block in bit_block:
+            left_list, right_list = list(), list()
+            left,right = self.seperation_list(block,32)
+            left_list.append(left)
+            right_list.append(right)
+            for i in range(16):
+                left_list.append(self.xor(right_list[i],self.F_function(left_list[i], self.generate_key(self.password,15 - i))))
+                right_list.append(left_list[i])           
+            text_end.extend(self.permut(self.combine_blocks([left_list[-1],right_list[-1]]), self.IP_1))
+            left_list.clear()
+            right_list.clear()
+        return self.byte_to_string(text_end)
 
     #Разделяет лист на блок листов нужной длины
     def seperation_list(self,lst, step):
@@ -174,25 +194,59 @@ class des():
         return cb
 
     """возвращает лист [B[0]B[1]..B[8]]"""
-    def Feistels_function(self,bit,key):
-        new_bit = permut(bit,self.E)
-        new_key = key        
-        B = self.seperation_list([new_bit[x] ^ new_bit[x] if len(new_bit) == len(new_key) else print('Длина расширенного ключа и длина битов не совпадает') for x in range(len(new_bit))],6)
+    def F_function(self,bit,key):
+        new_bit = self.permut(bit,self.E)
+        new_key = key 
+        B = self.seperation_list([new_bit[x] ^ new_key[x] for x in range(len(new_bit))],6)
+        S = list()
         #""" операция хор между строкой и ключой и разбиение на список по 6 бит"""
-        for i in range(B):
-            nb_row = int(''.join(str(b) for b in [[B[i]][0],[B[i]][-1]] ),2)
-            nb_column = int(''.join(str(b) for b in [B[i]][1:-1] ),2)
-            B[i] = self.string_to_byte([[S_BOX[i]][nb_row]][nb_column])
-        return self.permut(self.combine_blocks(B), self.P)
+        result = list()
+        for i in range(len(B)):
+            ############
+            b = B[i]
+            nb_row = int((lambda x,y: str(x) + str(y))(b[0],b[-1]),2)
+            nb_column = int(''.join([str(s) for s in b[1:-1]]),2)
+            s = self.S_BOX[i]
+            s_nb_row = s[nb_row]
+            S.append(self.int_to_bit(s_nb_row[nb_column],4))
+        return self.permut(self.combine_blocks(S), self.P)
         
     def generate_key(self, key, iteration):
-        new_key = permut(key,self.G)
-        c,d = seperation_list(new_key,28)
-        c,d = self.shift(c,d,SHIFT[iteration])
+        new_key = self.permut(key,self.G)
+        c,d = self.seperation_list(new_key,28)
+        c,d = self.shift(c,d,self.SHIFT[iteration])
         return self.permut(c + d, self.H)
 
     #сдвиг листа на определенное значение
     def shift(self, c, d, n): 
         return c[n:] + c[:n], d[n:] + d[:n]
 
-    #def expand(self,bit,block):
+    def xor(self,list_one, list_two):
+        s = []
+        if len(list_one) == len(list_two):
+            return [x ^ y for x,y in zip(list_one, list_two)]
+        else :
+            print("Error: ")
+
+    def int_to_bit(self,number, length):
+        string = bin(number)[2:]
+        string = str(string).zfill(length)
+        bitList = list()
+        for byte in string:
+            if byte == 'b': 
+                byte = '0'
+            bitList.append(int(byte))
+        return bitList
+
+class cbc(des):
+    pass
+
+if __name__ == '__main__':
+    key = "secret_k"
+    textD= "hello world"
+    d = des()
+    r = d.encrypt(textD,key)
+    k = d.decrypt(r,key)
+    print("Original text: ",textD)
+    print("Cipher text: ",r)
+    print("Decrypt text: ",k)
